05f929ca9bc5335226282823e25570f7e6040dc0
Appsaloon Toon Nelissen <toon@appsaloon.be>
2017-11-06 19:06:22 +0100

    refactoring

--- server.js
+++ server.js
@@ -1,98 +1,74 @@
-'use strict';
+/*eslint no-undef: "error"*/
+/*eslint-env node*/
+'use strict'
 
-const express = require('express');
-var ttn = require("ttn")
+const request = require('request')
+const ttn = require('ttn')
 
-var request = require('request');
-
-var options;
-
-function callback(error, response, body) {
-  if (!error && response.statusCode == 200) {
-    var info = JSON.parse(body);
-    console.log(info.stargazers_count + " Stars");
-    console.log(info.forks_count + " Forks");
-  }
+const log = (logMessage) => {
+    process.stdout.write(logMessage + '\n')
 }
 
-console.log(process.env);
+if (process.env.appID === undefined || process.env.accessKey === undefined || process.env.prefix === undefined) {
+    log('Provide \'appID\' & \'accessKey\' & \'prefix\' for your ttn application with the environment')
+    process.exit(1)
+}
 
-// Constants
-const PORT = 8080;
-const HOST = '0.0.0.0';
+const appID = process.env.appID
+const accessKey = process.env.accessKey
+const prefix = process.env.prefix
 
-// App
-const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello world\n');
-});
+const sendData = (payload, deviceId) => {
+    const options = {
+        headers: {
+            'X-Pin': (payload[0] === 'P1' )? 1:7,
+            'X-Sensor': deviceId,
+        },
+        method: 'POST',
+        url: '',
+        json: {
+            software_version : 'TTNBXL-v1',
+            sensordatavalues : payload
+        }
+    }
+    // send data to madavi
+    options.url = 'https://api-rrd.madavi.de/data.php'
+    request(options)
+    // send data to luftdaten
+    options.url = 'https://api.luftdaten.info/v1/push-sensor-data/'
+    request(options)
+}
 
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
-
-const appID = ""
-const accessKey = ""
-
-// discover handler and open mqtt connection
 ttn.data(appID, accessKey)
-  .then(function (client) {
-    client.on("uplink", function (devID, payload) {
+    .then((client) => {
+        client.on('uplink', (devID, payload) => {
+            log('Received data from device: '+ devID)
 
-      if (payload.dev_id.indexOf("toon_n_") > -1) {
-        console.log("Received uplink from ", devID)
-        console.log(payload.payload_fields)
-        console.log(payload.metadata.gateways)
+            // temp condition for testing only
+            if (payload.dev_id.indexOf('toon_n_') > -1) {
 
-        options = {
-          headers: {
-            'X-Pin': 1,
-            'X-Sensor': 'TTNBXL-'+parseInt(payload.hardware_serial,16)
-          },
-          method: 'POST',
-          url: 'https://api-rrd.madavi.de/data.php',
-          json: {
-            software_version : 'TTNBXL-v1',
-            sensordatavalues : [
-              {value_type: 'P1', value: payload.payload_fields.pm10.toString()},
-              {value_type: 'P2', value: payload.payload_fields.pm25.toString()}
-            ]
-          }
-        }
-        request(options);
-        options = {
-          headers: {
-            'X-Pin': 7,
-            'X-Sensor': 'TTNBXL-'+parseInt(payload.hardware_serial,16)
-          },
-          method: 'POST',
-          url: 'https://api-rrd.madavi.de/data.php',
-          json: {
-            software_version : 'TTNBXL-v1',
-            sensordatavalues : [
-              {value_type: 'temperature', value: payload.payload_fields.temperature.toString()},
-              {value_type: 'humidity', value: payload.payload_fields.humidity.toString()}
-            ]
-          }
-        }
-        request(options);
-      }
+                const deviceId = prefix + parseInt(payload.hardware_serial,16)
+
+                if (payload.payload_fields.pm10 && payload.payload_fields.pm25) {
+                    const pmPayload = [
+                        {value_type: 'P1', value: payload.payload_fields.pm10.toString()},
+                        {value_type: 'P2', value: payload.payload_fields.pm25.toString()}
+                    ]
+                    sendData(pmPayload, deviceId)
+                }
+
+                if (payload.payload_fields.temperature && payload.payload_fields.humidity) {
+                    const dhtPayload = [
+                        {value_type: 'temperature', value: payload.payload_fields.temperature.toString()},
+                        {value_type: 'humidity', value: payload.payload_fields.humidity.toString()}
+                    ]
+                    sendData(dhtPayload, deviceId)
+                }
+                
+            }
+        })
     })
-  })
-  .catch(function (err) {
-    console.error(err)
-    process.exit(1)
-  })
-
-// discover handler and open application manager client
-/*ttn.application(appID, accessKey)
-  .then(function (client) {
-    return client.get()
-  })
-  .then(function (app) {
-    console.log("Got app", app)
-  })
-  .catch(function (err) {
-    console.error(err)
-    process.exit(1)
-  })
-*/
+    .catch(function (err) {
+        log(err)
+        process.exit(1)
+    })
